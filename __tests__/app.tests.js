906358491b7375e86d3bfdc037b9953f8c3fdafc
bastiengrignon <basgrignon@gmail.com>
2020-12-01 16:26:53 +0100

    ADD API tests

---
+++ __tests__/app.tests.js
@@ -0,0 +1,24 @@
+import request from "supertest";
+import app from "../app";
+
+describe("API tests", () => {
+    it('should return corresponding songs', async () => {
+        const searchedSong = "Pain"
+
+        const res = await request(app).get(`/${searchedSong}`)
+
+        expect(res.status).toBe(200)
+        expect(res.body).toEqual({
+            songs: ["Pain Is So Close to Pleasure"]
+        })
+    });
+
+    it('should return 404 error', async () => {
+        const searchedSong = ""
+
+        const res = await request(app).get(`/${searchedSong}`)
+
+        expect(res.status).toBe(404)
+        expect(res.body).toEqual({})
+    });
+})
