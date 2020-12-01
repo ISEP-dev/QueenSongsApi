034d6ee0964b91d299c8c252bedaddf210653173
bastiengrignon <basgrignon@gmail.com>
2020-12-01 16:26:16 +0100

    FIX indentation

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import {allSongs} from "./songs";
+import {allSongs} from "./songs"
 
 const app = express()
 
 app.use(bodyParser.json())
-app.use(function(_req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
+app.use(function (_req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
 })
 
 app.get('/:text?', (req, res) => {
-  const text = req.params.text
-  if (!text) {
-    return res.status(404)
-  }
+    const text = req.params.text
+    if (!text) {
+        return res.status(404)
+    }
 
-  const songs = allSongs.filter(s => s.toLowerCase().includes(text.toLowerCase()));
-  return res.status(200).set({ 'Content-Type': 'application/json' }).json({songs});
+    const songs = allSongs.filter(s => s.toLowerCase().includes(text.toLowerCase()))
+    return res.status(200).set({'Content-Type': 'application/json'}).json({songs})
 })
 
 export default app
